4535a3ff2638195374c0b14e8673acb65ff33503
UKRAINE20 <150735835+UKRAINE20@users.noreply.github.com>
2024-02-14 22:24:17 +0000

    sort_mass

--- .ccls-cache/@home@runner@myfirstproject/main.cpp
+++ .ccls-cache/@home@runner@myfirstproject/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm> // для функції sort()
 
 using namespace std;
 
 int main() {
-    int arraySize; 
-    cout << "Введіть розмір масиву: ";
-    cin >> arraySize; 
+    int arraySize; // Змінна для розміру масиву
 
-    int myArray[arraySize]; 
+    // Перевіряємо введення користувача на валідність
+    do {
+        cout << "Введіть розмір масиву (більше 0): ";
+        cin >> arraySize;
+    } while (arraySize <= 0);
+
+    int myArray[arraySize]; // Оголошення масиву
 
     srand(time(0));
 
-    
+    // Заповнюємо масив випадковими числами
     for (int i = 0; i < arraySize; ++i) {
-        myArray[i] = rand() % 100; 
+        myArray[i] = rand() % 100; // Генеруємо випадкове число від 0 до 99
     }
 
-    cout << "Масив: ";
+    // Сортуємо масив
+    sort(myArray, myArray + arraySize);
+
+    // Виводимо масив на екран
+    cout << "Відсортований масив: ";
     for (int i = 0; i < arraySize; ++i) {
         cout << myArray[i] << " ";
     }
